c053e36c36a0c568b70cd5bf0b5329a06341d6b6
LENOVO-YOGA\alfino-juliendo <alfinojuliendo@gmail.com>
2022-01-26 15:43:16 +0700

    Restructure package

--- app/src/main/java/com/demo/newsapp/featureNews/presentation/NewsActivity.kt
+++ app/src/main/java/com/demo/newsapp/featureNews/presentation/NewsActivity.kt
@@ -1,11 +1,12 @@
-package com.demo.newsapp
+package com.demo.newsapp.featureNews.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.demo.newsapp.R
 
-class MainActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_news)
     }
 }
